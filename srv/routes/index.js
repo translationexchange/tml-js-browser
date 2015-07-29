b5d0344da431f4d38bbe2a9cb80f6856acb5d162
Ian McDaniel <ianmcdaniel@gmail.com>
2015-07-29 09:22:54 -0700

    added ignoreElements method

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -1,4 +1,8 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
+var pckg = require('../../package.json');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -7,8 +11,17 @@ router.get('/', function(req, res) {
 });
 
 router.get('/tml.js', function(req, res) {
-  res.redirect('/index.html');
+  fs.readFile(path.join(__dirname, '../../dist', 'tml-'+pckg.version+'.js'), 'utf8', function(err,data){
+    res.send(data);  
+  });
 });
+
+router.get('/tml.min.js', function(req, res) {
+  fs.readFile(path.join(__dirname, '../../dist', 'tml-'+pckg.version+'.min.js'), 'utf8', function(err,data){
+    res.send(data);  
+  });
+});
+
 
 
 module.exports = router;
