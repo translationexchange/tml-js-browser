96686cb2ed96abff02bf5cb4c9e9ad09468d93c4
Michael Berkovich <michael@translationexchange.com>
2016-03-04 17:37:51 -0800

    Updated distribution for version 0.4.40

--- dist/tml.js
+++ dist/tml.js
@@ -798,7 +798,7 @@ module.exports = {
     var mutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 
     tml = tml.utils.extend(tml, {
-      version: '0.4.39',
+      version: '0.4.40',
 
       on: emitter.on.bind(emitter),
       off: emitter.off.bind(emitter),
