f5a13657a2c191b6a2afb84f24557a263a8c8c11
Ian McDaniel <ianmcdainel@gmail.com>
2016-09-27 12:52:12 -0700

    added attribute translation

--- dist/tml.js
+++ dist/tml.js
@@ -1728,6 +1728,7 @@ module.exports = {
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+ 
 
 var tml         = require('tml-js');
 var config      = tml.config;
@@ -1775,9 +1776,15 @@ DomTokenizer.prototype = {
    * Updates all nodes with translations
    */
   updateAllNodes: function(){
+    var item;
     for(var i=0,l=this.contentCache.length;i<l;i++){
-      if(this.contentCache[i].container) {
-        this.contentCache[i].container.innerHTML = this.translateTml(this.contentCache[i].tml, this.contentCache[i].data);
+      item = this.contentCache[i];
+      if(item.container) {
+        if(item.attribute) {
+          item.container.setAttribute(item.attribute, this.translateTml(item.tml, item.data, false));  
+        } else {
+          item.container.innerHTML = this.translateTml(item.tml, item.data);  
+        }
       }
     }
   },
@@ -1826,16 +1833,20 @@ DomTokenizer.prototype = {
   },
 
 
+
+
+
   /**
    * Translates DOM
    *
    * @param node
    */
   translateDOM: function(node) {
+    
     if(this.translatedNodes.indexOf(node) !== -1) return;
     this.translatedNodes.push(node);
 
-    if (node.nodeType == 3) { return; }
+    if (node.nodeType == 3) {return; }
 
     var source = node.nodeType == 1 && this.getSourceBlock(node);
     if (source) {
@@ -1867,7 +1878,47 @@ DomTokenizer.prototype = {
     if(source) {
       window.tml_end_block();
     }
+
+    if(node.getAttribute) {
+      this.translateAttributes(node);  
+    }
   },
+
+
+
+  /**
+   * Translates Node Attibutes
+   *
+   * @param node
+   */
+  translateAttributes: function(node){
+    var val, self = this;
+    config.translator_options.attributes.labels.forEach(function(attr){
+      if(node.hasAttribute(attr)) {self.replaceAttributes(node, attr);}
+    });
+  },
+
+
+  /**
+   * Translates Node Attibutes
+   *
+   * @param node
+   * @param attr
+   */
+  replaceAttributes: function(node, attr){
+    var tml         = node.getAttribute(attr);
+    // console.log(66, this.tokens);
+    var data        = this.tokens;
+    var translation = this.translateTml(tml, null, true);
+    // console.log(77, this.tokens);
+    
+
+    console.log(44, tml, data);
+
+    node.setAttribute(attr, translation);
+    this.contentCache.push({container: node, tml: tml, data: data, attribute: attr}); 
+  },
+
 
   /**
    * Determine a source block
@@ -1912,7 +1963,7 @@ DomTokenizer.prototype = {
    * @param data
    * @returns {*}
    */
-  translateTml: function(tml, data) {
+  translateTml: function(tml, data, label) {
     tml = this.generateDataTokens(tml);
     data = data || (this.tokens);
 
@@ -1925,7 +1976,7 @@ DomTokenizer.prototype = {
       if (sentences) {
         var self = this;
         sentences.forEach(function (sentence) {
-          var sentenceTranslation = self.getOption("debug") ? self.debugTranslation(sentence) : window.tr(sentence, data, self.options);
+          var sentenceTranslation = self.getOption("debug") ? self.debugTranslation(sentence) : window[label?"trl":"tr"](sentence, data, self.options);
           translation = translation.replace(sentence, sentenceTranslation);
         });
         this.resetContext();
@@ -2274,11 +2325,9 @@ module.exports = DomTokenizer;
  */
 /* eslint-disable no-proto */
 
-'use strict'
-
 var base64 = require('base64-js')
 var ieee754 = require('ieee754')
-var isArray = require('isarray')
+var isArray = require('is-array')
 
 exports.Buffer = Buffer
 exports.SlowBuffer = SlowBuffer
@@ -2316,22 +2365,20 @@ var rootParent = {}
  */
 Buffer.TYPED_ARRAY_SUPPORT = global.TYPED_ARRAY_SUPPORT !== undefined
   ? global.TYPED_ARRAY_SUPPORT
-  : typedArraySupport()
-
-function typedArraySupport () {
-  function Bar () {}
-  try {
-    var arr = new Uint8Array(1)
-    arr.foo = function () { return 42 }
-    arr.constructor = Bar
-    return arr.foo() === 42 && // typed array instances can be augmented
-        arr.constructor === Bar && // constructor can be set
-        typeof arr.subarray === 'function' && // chrome 9-10 lack `subarray`
-        arr.subarray(1, 1).byteLength === 0 // ie10 has broken `subarray`
-  } catch (e) {
-    return false
-  }
-}
+  : (function () {
+      function Bar () {}
+      try {
+        var arr = new Uint8Array(1)
+        arr.foo = function () { return 42 }
+        arr.constructor = Bar
+        return arr.foo() === 42 && // typed array instances can be augmented
+            arr.constructor === Bar && // constructor can be set
+            typeof arr.subarray === 'function' && // chrome 9-10 lack `subarray`
+            arr.subarray(1, 1).byteLength === 0 // ie10 has broken `subarray`
+      } catch (e) {
+        return false
+      }
+    })()
 
 function kMaxLength () {
   return Buffer.TYPED_ARRAY_SUPPORT
@@ -2358,10 +2405,8 @@ function Buffer (arg) {
     return new Buffer(arg)
   }
 
-  if (!Buffer.TYPED_ARRAY_SUPPORT) {
-    this.length = 0
-    this.parent = undefined
-  }
+  this.length = 0
+  this.parent = undefined
 
   // Common case.
   if (typeof arg === 'number') {
@@ -2492,10 +2537,6 @@ function fromJsonObject (that, object) {
 if (Buffer.TYPED_ARRAY_SUPPORT) {
   Buffer.prototype.__proto__ = Uint8Array.prototype
   Buffer.__proto__ = Uint8Array
-} else {
-  // pre-set for values that may exist in the future
-  Buffer.prototype.length = undefined
-  Buffer.prototype.parent = undefined
 }
 
 function allocate (that, length) {
@@ -2645,6 +2686,10 @@ function byteLength (string, encoding) {
   }
 }
 Buffer.byteLength = byteLength
+
+// pre-set for values that may exist in the future
+Buffer.prototype.length = undefined
+Buffer.prototype.parent = undefined
 
 function slowToString (encoding, start, end) {
   var loweredCase = false
@@ -3287,7 +3332,7 @@ Buffer.prototype.writeUInt8 = function writeUInt8 (value, offset, noAssert) {
   offset = offset | 0
   if (!noAssert) checkInt(this, value, offset, 1, 0xff, 0)
   if (!Buffer.TYPED_ARRAY_SUPPORT) value = Math.floor(value)
-  this[offset] = (value & 0xff)
+  this[offset] = value
   return offset + 1
 }
 
@@ -3304,7 +3349,7 @@ Buffer.prototype.writeUInt16LE = function writeUInt16LE (value, offset, noAssert
   offset = offset | 0
   if (!noAssert) checkInt(this, value, offset, 2, 0xffff, 0)
   if (Buffer.TYPED_ARRAY_SUPPORT) {
-    this[offset] = (value & 0xff)
+    this[offset] = value
     this[offset + 1] = (value >>> 8)
   } else {
     objectWriteUInt16(this, value, offset, true)
@@ -3318,7 +3363,7 @@ Buffer.prototype.writeUInt16BE = function writeUInt16BE (value, offset, noAssert
   if (!noAssert) checkInt(this, value, offset, 2, 0xffff, 0)
   if (Buffer.TYPED_ARRAY_SUPPORT) {
     this[offset] = (value >>> 8)
-    this[offset + 1] = (value & 0xff)
+    this[offset + 1] = value
   } else {
     objectWriteUInt16(this, value, offset, false)
   }
@@ -3340,7 +3385,7 @@ Buffer.prototype.writeUInt32LE = function writeUInt32LE (value, offset, noAssert
     this[offset + 3] = (value >>> 24)
     this[offset + 2] = (value >>> 16)
     this[offset + 1] = (value >>> 8)
-    this[offset] = (value & 0xff)
+    this[offset] = value
   } else {
     objectWriteUInt32(this, value, offset, true)
   }
@@ -3355,7 +3400,7 @@ Buffer.prototype.writeUInt32BE = function writeUInt32BE (value, offset, noAssert
     this[offset] = (value >>> 24)
     this[offset + 1] = (value >>> 16)
     this[offset + 2] = (value >>> 8)
-    this[offset + 3] = (value & 0xff)
+    this[offset + 3] = value
   } else {
     objectWriteUInt32(this, value, offset, false)
   }
@@ -3408,7 +3453,7 @@ Buffer.prototype.writeInt8 = function writeInt8 (value, offset, noAssert) {
   if (!noAssert) checkInt(this, value, offset, 1, 0x7f, -0x80)
   if (!Buffer.TYPED_ARRAY_SUPPORT) value = Math.floor(value)
   if (value < 0) value = 0xff + value + 1
-  this[offset] = (value & 0xff)
+  this[offset] = value
   return offset + 1
 }
 
@@ -3417,7 +3462,7 @@ Buffer.prototype.writeInt16LE = function writeInt16LE (value, offset, noAssert) 
   offset = offset | 0
   if (!noAssert) checkInt(this, value, offset, 2, 0x7fff, -0x8000)
   if (Buffer.TYPED_ARRAY_SUPPORT) {
-    this[offset] = (value & 0xff)
+    this[offset] = value
     this[offset + 1] = (value >>> 8)
   } else {
     objectWriteUInt16(this, value, offset, true)
@@ -3431,7 +3476,7 @@ Buffer.prototype.writeInt16BE = function writeInt16BE (value, offset, noAssert) 
   if (!noAssert) checkInt(this, value, offset, 2, 0x7fff, -0x8000)
   if (Buffer.TYPED_ARRAY_SUPPORT) {
     this[offset] = (value >>> 8)
-    this[offset + 1] = (value & 0xff)
+    this[offset + 1] = value
   } else {
     objectWriteUInt16(this, value, offset, false)
   }
@@ -3443,7 +3488,7 @@ Buffer.prototype.writeInt32LE = function writeInt32LE (value, offset, noAssert) 
   offset = offset | 0
   if (!noAssert) checkInt(this, value, offset, 4, 0x7fffffff, -0x80000000)
   if (Buffer.TYPED_ARRAY_SUPPORT) {
-    this[offset] = (value & 0xff)
+    this[offset] = value
     this[offset + 1] = (value >>> 8)
     this[offset + 2] = (value >>> 16)
     this[offset + 3] = (value >>> 24)
@@ -3462,7 +3507,7 @@ Buffer.prototype.writeInt32BE = function writeInt32BE (value, offset, noAssert) 
     this[offset] = (value >>> 24)
     this[offset + 1] = (value >>> 16)
     this[offset + 2] = (value >>> 8)
-    this[offset + 3] = (value & 0xff)
+    this[offset + 3] = value
   } else {
     objectWriteUInt32(this, value, offset, false)
   }
@@ -3737,7 +3782,7 @@ function utf8ToBytes (string, units) {
       }
 
       // valid surrogate pair
-      codePoint = (leadSurrogate - 0xD800 << 10 | codePoint - 0xDC00) + 0x10000
+      codePoint = leadSurrogate - 0xD800 << 10 | codePoint - 0xDC00 | 0x10000
     } else if (leadSurrogate) {
       // valid bmp char, but last char was a lead
       if ((units -= 3) > -1) bytes.push(0xEF, 0xBF, 0xBD)
@@ -3816,7 +3861,7 @@ function blitBuffer (src, dst, offset, length) {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"base64-js":9,"ieee754":10,"isarray":11}],9:[function(require,module,exports){
+},{"base64-js":9,"ieee754":10,"is-array":11}],9:[function(require,module,exports){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
 ;(function (exports) {
@@ -4029,10 +4074,38 @@ exports.write = function (buffer, value, offset, isLE, mLen, nBytes) {
 }
 
 },{}],11:[function(require,module,exports){
-var toString = {}.toString;
 
-module.exports = Array.isArray || function (arr) {
-  return toString.call(arr) == '[object Array]';
+/**
+ * isArray
+ */
+
+var isArray = Array.isArray;
+
+/**
+ * toString
+ */
+
+var str = Object.prototype.toString;
+
+/**
+ * Whether or not the given `val`
+ * is an array.
+ *
+ * example:
+ *
+ *        isArray([]);
+ *        // > true
+ *        isArray(arguments);
+ *        // > false
+ *        isArray('');
+ *        // > false
+ *
+ * @param {mixed} val
+ * @return {bool}
+ */
+
+module.exports = isArray || function (val) {
+  return !! val && '[object Array]' == str.call(val);
 };
 
 },{}],12:[function(require,module,exports){
@@ -4221,7 +4294,7 @@ module.exports = {
       splitters:  ["br", "hr"]
     },
     attributes: {
-      labels: ["title", "alt"]
+      labels: ["title", "alt", "placeholder"]
     },
     name_mapping: {
       b: "bold",
@@ -6145,7 +6218,12 @@ Application.prototype = {
    */
   getDefaultLanguage: function() {
     var language = this.getLanguage(this.default_locale);
-    language = language || new Language(config.getDefaultLanguage());
+    if (!language) {
+      if (!this.configDefaultLanguage)
+        this.configDefaultLanguage = new Language(config.getDefaultLanguage());
+      
+      language = this.configDefaultLanguage;
+    }
     language.application = this;
     return language;
   },
@@ -6259,17 +6337,20 @@ Application.prototype = {
       }
 
       self.loadExtension(data);
+      
+      // /es-LA/article
+      // self.current_locale = (
+      //   options.current_locale ||
+      //   self.getPreferredLocale(options.accepted_locales, self.languages) ||
+      //   self.default_locale
+      // );
+      //
+      // if (!self.isSupportedLocale(self.current_locale)) {
+      //   self.current_locale = self.default_locale;
+      // }
 
-      self.current_locale = (
-        options.current_locale ||
-        self.getPreferredLocale(options.accepted_locales, self.languages) ||
-        self.default_locale
-      );
-
-      if (!self.isSupportedLocale(self.current_locale)) {
-        self.current_locale = self.default_locale;
-      }
-
+      self.current_locale = self.getSupportedLocale(options.current_locale, options.accepted_locales, self.default_locale);
+      
       var locales = [self.default_locale];
       if (self.current_locale != self.default_locale) {
         locales.push(self.current_locale);
@@ -6282,6 +6363,21 @@ Application.prototype = {
     });
   },
 
+  /**
+   * 
+   * @param requested_locale
+   * @param accepted_locales
+   * @param default_locale
+   * @returns {*}
+   */
+  getSupportedLocale: function(requested_locale, accepted_locales, default_locale) {
+    // /es-LA/article
+    if (this.isSupportedLocale(requested_locale))
+      return requested_locale;
+    
+    return this.getPreferredLocale(accepted_locales, this.languages) || default_locale;
+  },
+  
   /**
    * Checks if the locale is part of the application
    *
@@ -7397,7 +7493,7 @@ var HTMLDecorator = {
     }
 
     var html = [];
-    html.push("<" + element + " class='" + classes.join(' ') + "' data-translation_key='" + translation_key.key + "' data-target_locale='" + target_language.locale + "'>");
+    html.push("<" + element + " class='" + classes.join(' ') + "' data-translation_key='" + translation_key.key + "' draggable='true' data-target_locale='" + target_language.locale + "'>");
     html.push(translated_label);
     html.push("</" + element + ">");
     return html.join("");
@@ -7691,7 +7787,9 @@ Language.prototype = {
 
   translate: function(label, description, tokens, options) {
     var params = utils.normalizeParams(label, description, tokens, options);
-
+    if (!params.label)
+      return '';
+    
     var translation_key = new TranslationKey({
       label:        params.label,
       description:  params.description,
